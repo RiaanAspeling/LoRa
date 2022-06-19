d7dcab986d93bf40e262db3a457f31d912ef8679
Riaan Aspeling <riaan@aspeling.co.za>
2022-06-19 16:18:13 +0200

    Extension functions for bit manipulation

---
+++ Utils/BitHandler.cs
@@ -0,0 +1,25 @@
+namespace LoRa.Utils
+{
+    public static class BitHandler
+    {
+        public static int ReadBit(this int value, int bitPosistion)
+        {
+            return (value & (1 << bitPosistion)) > 0 ? 1 : 0;
+        }
+
+        public static int ReadBitRange(this int value, int startPosition, int length)
+        {
+            return (value & BuildBitMask(startPosition, length)) >> startPosition;
+        }
+
+        public static int BuildBitMask(int startPos, int length)
+        {
+            var rtn = 0;
+            for (int i = 0; i <= length - 1; i++)
+            {
+                rtn += (1 << i);
+            }
+            return rtn << startPos;
+        }
+    }
+}
